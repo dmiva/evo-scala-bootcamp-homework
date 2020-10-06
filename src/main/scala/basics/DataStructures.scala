2731189e9a4580b131820368b54aab4ce950e9df
Dmitrijs Ivančenko <dmitry1991@gmail.com>
2020-10-07 01:42:21 +0300

    First working version HW4

--- src/main/scala/basics/DataStructures.scala
+++ src/main/scala/basics/DataStructures.scala
@@ -22,52 +22,44 @@ object DataStructures {
   def sortConsideringEqualValues[T](map: Map[T, Int]): List[(Set[T], Int)] = ???
 
   def main(args: Array[String]): Unit = {
+
     val myMap = Map("a" -> 1, "b" -> 2, "c" -> 4, "d" -> 1, "e" -> 0, "f" -> 2, "g" -> 2)
 
     val myList = List(Set("e") -> 0, Set("a", "d") -> 1, Set("b", "f", "g") -> 2, Set("c") -> 4)
 
+    println("Map: " + myMap)
     println("Expected: " + myList)
 
-    println("Map: " + myMap)
 
-    val mapCollect = myMap.toList.collect({
-      case (key, value) => (value, key)
-    })
-
+//    val mapCollect = myMap.toList.collect({
+//      case (key, value) => (value, key)
+//    })
 
 
 
-    // 1. Go through map and
-//    val myMapInverted = for {
-//      keys <- myMap.keys
-//      values <- myMap.values
-//    } yield (values, keys)
-
-    println("Collected: " + mapCollect)
-    println("Sorted : " + mapCollect.sorted)
+//    println("Collected: " + mapCollect)
+//    println("Sorted : " + mapCollect.sorted)
 
 
-    val groupedList = mapCollect.sorted.groupBy(_._1)
-    println("Grouped : " + groupedList)
+//    val groupedList = mapCollect.sorted.groupBy(_._1)
+//    println("Grouped : " + groupedList)
 
     val mapSwapped = myMap.groupBy(_._2).map {
-      case (k, v) => (k -> v.keys)
+      case (k, v) => k -> v.keys.toList.sorted.toSet
     }.collect({
       case (key, value) => (value, key)
     }).toList.sortBy(_._2)
 
     println("Swapped: " + mapSwapped)
 
-    val list = List((0,"e"))
-    val list2 = List((1,"a"), (1,"d"), (2,"f"))
+//    val myPartFunc: PartialFunction[(String,Int),(Int,String)] = new PartialFunction[(String,Int),(Int,String)] {
+//      override def isDefinedAt(x: (String, Int)): Boolean = true
+//
+//      override def apply(v1: (String, Int)): (Int, String) = v1 match {
+//        case (str, i) => (i, str)
+//      }
+//    }
 
-    val list3 = groupedList.map {
-      case (i, str) => str map {
-        case (i, str) => str
-      }
-    }
-
-    println(list3)
     //    println(mapCollect.sorted.groupBy(_._1))
 
 //
