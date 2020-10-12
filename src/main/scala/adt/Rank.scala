f549a41b228d429c198a084ff432528b1f862f10
Dmitrijs Ivančenko <dmitry1991@gmail.com>
2020-10-12 03:01:27 +0300

    Created Suit and Rank using traits.

--- src/main/scala/adt/Rank.scala
+++ src/main/scala/adt/Rank.scala
@@ -1,11 +1,76 @@
 package adt
 
-sealed abstract case class Rank private(value: Char)
+//sealed abstract case class Rank private(value: Char)
+//object Rank {
+//  def create(value: Char): Either[String, Rank] = value match {
+//    case '2' | '3' | '4' | '5' | '6' |
+//         '7' | '8' | '9' | 'T' | 'J' |
+//         'Q' | 'K' | 'A'    => Right(new Rank(value) {})
+//    case _                  => Left(s"Rank symbol not correct: '$value'")
+//  }
+//}
+
+sealed trait Rank {
+  def value: Char
+}
+
 object Rank {
-  def create(value: Char): Either[String, Rank] = value match {
-    case '2' | '3' | '4' | '5' | '6' |
-         '7' | '8' | '9' | 'T' | 'J' |
-         'Q' | 'K' | 'A'    => Right(new Rank(value) {})
-    case _                  => Left(s"Rank symbol not correct: '$value'")
+  final case object Two extends Rank {
+    def value = '2'
   }
+  final case object Three extends Rank {
+    def value = '3'
+  }
+  final case object Four extends Rank {
+    def value = '4'
+  }
+  final case object Five extends Rank {
+    def value = '5'
+  }
+  final case object Six extends Rank {
+    def value = '6'
+  }
+  final case object Seven extends Rank {
+    def value = '7'
+  }
+  final case object Eight extends Rank {
+    def value = '8'
+  }
+  final case object Nine extends Rank {
+    def value = '9'
+  }
+  final case object Ten extends Rank {
+    def value = 'T'
+  }
+  final case object Jack extends Rank {
+    def value = 'J'
+  }
+  final case object Queen extends Rank {
+    def value = 'Q'
+  }
+  final case object King extends Rank {
+    def value = 'K'
+  }
+  final case object Ace extends Rank {
+    def value = 'A'
+  }
+
+  // Smart constructor
+  def create(value: Char): Either[String, Rank] = value match {
+    case '2' => Right(Two)
+    case '3' => Right(Three)
+    case '4' => Right(Four)
+    case '5' => Right(Five)
+    case '6' => Right(Six)
+    case '7' => Right(Seven)
+    case '8' => Right(Eight)
+    case '9' => Right(Nine)
+    case 'T' => Right(Ten)
+    case 'J' => Right(Jack)
+    case 'Q' => Right(Queen)
+    case 'K' => Right(King)
+    case 'A' => Right(Ace)
+    case _   => Left(s"Rank symbol not correct: '$value'")
+  }
+
 }
--- src/main/scala/adt/Suit.scala
+++ src/main/scala/adt/Suit.scala
@@ -1,9 +1,39 @@
 package adt
 
-sealed abstract case class Suit private(value: Char)
-object Suit {
-  def create(value: Char): Either[String, Suit] = value match {
-    case 'c' | 'd' | 'h' | 's' => Right(new Suit(value) {})
-    case _ => Left(s"Suit symbol not correct: '$value'")
-  }
+//sealed abstract case class Suit private(value: Char)
+//object Suit {
+//  def create(value: Char): Either[String, Suit] = value match {
+//    case 'c' | 'd' | 'h' | 's' => Right(new Suit(value) {})
+//    case _ => Left(s"Suit symbol not correct: '$value'")
+//  }
+//}
+
+sealed trait Suit {
+  def value: Char
 }
+
+object Suit {
+  final case object Clubs extends Suit {
+    def value = 'c'
+  }
+  final case object Diamonds extends Suit {
+    def value = 'd'
+  }
+  final case object Hearts extends Suit {
+    def value = 'h'
+  }
+  final case object Spades extends Suit {
+    def value = 's'
+  }
+
+  // Smart constructor
+  def create(value: Char): Either[String, Suit] = value match {
+    case 'c' => Right(Clubs)
+    case 'd' => Right(Diamonds)
+    case 'h' => Right(Hearts)
+    case 's' => Right(Spades)
+    case _   => Left(s"Suit symbol not correct: '$value'")
+  }
+
+}
+
